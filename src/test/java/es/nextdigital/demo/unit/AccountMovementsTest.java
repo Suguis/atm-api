28c40cdc38039ab561baf6b6a036709426189f24
Sugui <anaisusg@gmail.com>
2024-10-20 18:01:46 +0200

    refactor: using TestUtils on account movements test

--- src/test/java/es/nextdigital/demo/unit/AccountMovementsTest.java
+++ src/test/java/es/nextdigital/demo/unit/AccountMovementsTest.java
@@ -1,40 +1,31 @@
 package es.nextdigital.demo.unit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.math.BigDecimal;
-import java.util.HashSet;
-import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
-import es.nextdigital.demo.model.Account;
 import es.nextdigital.demo.model.AccountMovement;
 import es.nextdigital.demo.model.AccountMovementType;
-import es.nextdigital.demo.model.Iban;
 
 public class AccountMovementsTest {
     @Test
     void adding_movements_gets_them() {
-        var account = new Account(new Iban("ES6621000418401234567891"));
-        var movements = List.of(new AccountMovement(BigDecimal.valueOf(2.50), AccountMovementType.DEPOSIT),
-                new AccountMovement(BigDecimal.valueOf(-1.00), AccountMovementType.TRANSFER));
+        var account = TestUtils.getTestAccount();
+        var movement = new AccountMovement(BigDecimal.valueOf(5), AccountMovementType.DEPOSIT);
 
-        movements.forEach(m -> account.addMovement(m));
+        account.addMovement(movement);
 
-        var retrievedMovements = account.getMovements();
-        assertEquals(new HashSet<>(movements), new HashSet<>(retrievedMovements));
+        assertTrue(account.getMovements().contains(movement));
     }
 
     @Test
     void movements_are_constistent_with_balance() {
-        var account = new Account(new Iban("ES6621000418401234567891"));
-        var movements = List.of(new AccountMovement(BigDecimal.valueOf(2.50), AccountMovementType.DEPOSIT),
-                new AccountMovement(BigDecimal.valueOf(-1.00), AccountMovementType.TRANSFER));
+        var account = TestUtils.getTestAccount();
 
-        movements.forEach(m -> account.addMovement(m));
-
-        assertEquals(movements.stream().map(m -> m.getQuantity()).reduce(BigDecimal.ZERO, BigDecimal::add),
+        assertEquals(account.getMovements().stream().map(m -> m.getQuantity()).reduce(BigDecimal.ZERO, BigDecimal::add),
                 account.getBalance());
     }
 }
